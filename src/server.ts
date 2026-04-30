7ffa0811467714f49916ab32cffe0144a41352c6
AdityaTarale <tarale.adi@gmail.com>
2026-05-01 03:17:22 +0530

    add: express sever

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,21 @@
+import app from "./app.js";
 import { Config } from "./config/index.js";
 
-console.log(Config.PORT);
+const startSever = () => {
+    const PORT = Config.PORT;
+    try {
+        const server = app.listen(PORT, () => {
+            console.log(`Listening on port ${PORT}`);
+        });
+
+        server.on("error", (error) => {
+            console.error("Server error:", error);
+            process.exit(1);
+        });
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
+
+startSever();
